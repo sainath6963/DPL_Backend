a3c27de5ab6c7cd2c313370dffa863b17b70c146
sainath <sb9codes@gmail.com>
2025-03-25 04:35:58 +0530

    latest commit

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is listening on both IPv4 and IPv6 at port ${PORT}`);
 });
