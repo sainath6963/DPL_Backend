84bd3d30581c5ff7064d96ef7ff9c3845b1dbee9
sainath <sb9codes@gmail.com>
2025-03-25 13:00:56 +0530

    commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import userRouter from "./routes/userRoutes.js";
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+dbConnection();
 
 app.use(
   cors({
@@ -27,7 +28,5 @@ app.get("/", (req, res) => {
 // Use your existing routes
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
-
-dbConnection();
 
 export default app;
