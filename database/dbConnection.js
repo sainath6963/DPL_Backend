3e7dd656a7861a807cc749c319f5774473da9d67
sainath <sb9codes@gmail.com>
2025-03-25 18:04:07 +0530

    database url correct

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
   try {
-    await mongoose.connect(
-      "mongodb:mongodb+srv://sainathbalkawade7:sainathbalkawade7@cluster0.opneb.mongodb.net/?retryWrites=true",
-      {
-        dbName: "DPL_REGISTRATION",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "DPL_REGISTRATION",
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    });
     console.log(` Database connected successfully`);
   } catch (error) {
     console.error(`Database connection failed: ${error.message}`);
