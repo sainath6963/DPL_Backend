4c4d52ae547cad7615b38cae22551e0793313b56
sainath <sb9codes@gmail.com>
2025-05-20 10:39:17 +0530

    test server

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is listening on both IPv4 and IPv6 at port ${PORT}`);
+  console.log(`Server is listening on both IPv4 and IPv6  port ${PORT}`);
 });
